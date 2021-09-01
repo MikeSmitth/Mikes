86552921fd35cbb89b1442d3168cd67002895c17
Krafan <jannn.nieee@gmail.com>
2021-09-01 17:56:30 +0200

    CONTROLER

---
+++ The Pass/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    public float timeToMove = 0.6f;
+    public float tileSize;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.forward));
+        if (Input.GetKey(KeyCode.S) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.back));
+        if (Input.GetKey(KeyCode.A) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.left));
+        if (Input.GetKey(KeyCode.D) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.right));
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedtime = 0;
+
+        origPos = transform.position;
+        targetPos = origPos + direction * tileSize;
+
+        while(elapsedtime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedtime / timeToMove));
+            elapsedtime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
