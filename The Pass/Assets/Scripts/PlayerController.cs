23a456a613ee025301b481d3d0d5d0cbf6094357
Krafan <jannn.nieee@gmail.com>
2021-09-02 20:10:05 +0200

    Dzisiaj praca to spierdaca ci musk

--- The Pass/Assets/Scripts/PlayerController.cs
+++ The Pass/Assets/Scripts/PlayerController.cs
@@ -10,17 +10,26 @@ public class PlayerController : MonoBehaviour
     public float timeToMove = 0.6f;
     public float tileSize;
 
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) && !isMoving)
+        if (Input.GetKeyDown(KeyCode.W) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.forward));
-        if (Input.GetKey(KeyCode.S) && !isMoving)
+        if (Input.GetKeyDown(KeyCode.S) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.back));
-        if (Input.GetKey(KeyCode.A) && !isMoving)
+        if (Input.GetKeyDown(KeyCode.A) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.left));
-        if (Input.GetKey(KeyCode.D) && !isMoving)
+        if (Input.GetKeyDown(KeyCode.D) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.right));
+        if (Input.GetKeyDown("e"))
+        {
+            StartCoroutine(RotateM(Vector3.up * 90, 0.8f));
+        }
+        if (Input.GetKeyDown("q"))
+        {
+            StartCoroutine(RotateM(Vector3.up * -90, 0.8f));
+        }
     }
 
     private IEnumerator MovePlayer(Vector3 direction)
@@ -32,7 +41,7 @@ public class PlayerController : MonoBehaviour
         origPos = transform.position;
         targetPos = origPos + direction * tileSize;
 
-        while(elapsedtime < timeToMove)
+        while (elapsedtime < timeToMove)
         {
             transform.position = Vector3.Lerp(origPos, targetPos, (elapsedtime / timeToMove));
             elapsedtime += Time.deltaTime;
@@ -42,5 +51,17 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+    }
+
+    IEnumerator RotateM(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+        transform.rotation = toAngle;
     }
 }
